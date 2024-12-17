93a4953ef3a6760bd3850210c8328c198cb97ea2
mohamed <mohamed@gmail.com>
2024-12-17 15:48:40 +0100

    i mplement the person methods in class

--- src/Model/Class.java
+++ src/Model/Class.java
@@ -1,4 +1,27 @@
 package Model;
 
-public class Class {
+public class Class extends Person{
+
+
+
+
+    @Override
+    public boolean Display() {
+        return false;
+    }
+
+    @Override
+    public void Update(int Id, String f, String l, String Email, int Phone, int N_Class, String Trainer) {
+
+    }
+
+    @Override
+    public void Delete(int Id) {
+
+    }
+
+    @Override
+    public void Add(int Id, String f, String l, String Email, int Phone, int N_Class, String Trainer) {
+
+    }
 }

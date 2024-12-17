45b84ec15e65343f8186ae549c2eebe61d989616
mohamed <mohamed@gmail.com>
2024-12-17 15:24:58 +0100

    add update and delete and add function to override

--- src/Model/Student.java
+++ src/Model/Student.java
@@ -1,37 +1,87 @@
 package Model;
 
+
+import java.util.ArrayList;
+
 public class Student extends Person {
-    private static   String  Trainer;
+    private String  Trainer;
+    static ArrayList<Student> list = new ArrayList<>();
 
-    public Student(int id, String firstName, String lastName, String email, int phone, int n_Class,String trainer) {
+
+    public Student(){
+
+    }
+
+    public Student(int id, String firstName, String lastName, String email, int phone, int n_Class, String trainer) {
         super(id, firstName, lastName, email, phone, n_Class);
-        this.Trainer=trainer;
+        Trainer=trainer;
     }
-
-
 
 
 
     @Override
-    public void Display() {
+    public boolean Display() {
 
-        System.out.print( "  First Name : "+getFirstName()+"\n  Last Name : "+getLastName()
-                + "\n  Number phone : "+getPhone()+"\n  Email : "
-                +getEmail()+"\n  Trainer : "+Trainer+"\n  Class : "+ getN_Class());
+        if (list.isEmpty()){
+            System.out.println("\n\n   Ops No Student To Display ");
+            return false;
+        }
+        else {
+            for (int i=0;i< list.size();i++) {
+                System.out.print( "\n\n  First Name : "+list.get(i).getFirstName()+"\n  Last Name : "+list.get(i).getLastName()
+                        + "\n  Number phone : "+getPhone()+"\n  Email : "
+                        +list.get(i).getEmail()+"\n  Trainer : "+ list.get(i).Trainer+"\n  Class : "+list.get(i).getN_Class());
+            }
+
+            return true;
+        }
+
     }
 
     @Override
-    public void Update() {
+    public void Update(int Id, String f, String l, String Email, int Phone, int N_Class, String Trainer) {
 
+        boolean ishere=false;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (Id == list.get(i).getId()) {
+                list.add(Id,new Student(Id, f, l, Email, Phone, N_Class, Trainer));
+                ishere=true;
+            }
+
+        }
+
+        if (ishere){
+            System.out.println("\n\n   Your Student Update With Success");
+        }
+        else {
+            System.out.println("\n\n   Ops Not Found !");
+
+        }
     }
 
     @Override
-    public void Delete() {
+    public void Delete(int id) {
 
+        boolean found = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (id == list.get(i).getId()) {
+                id = i;
+                found = true;
+            }
+        }
+        if (found) {
+            list.remove(id);
+            System.out.println("\n\n      Deleted With Success");
+
+        } else {
+            System.out.println("\n\n      Ops Not Found");
+        }
     }
 
     @Override
-    public void Add() {
-
+    public  void Add(int Id, String f, String l, String Email, int Phone, int N_Class, String Trainer) {
+        list.add(new Student(Id, f, l, Email, Phone, N_Class, Trainer));
     }
+
 }

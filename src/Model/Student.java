83c075b2a0d3da0f934b3e3ab78b378b7362191f
mohamed <mohamed@gmail.com>
2024-12-17 12:16:06 +0100

    i make the student extend the person class to get shared data

--- src/Model/Student.java
+++ src/Model/Student.java
@@ -1,86 +1,20 @@
 package Model;
 
-public class Student {
-    private int Id;
-    private  String FirstName;
-    private  String  LastName;
-    private  String  Email;
-    private  String  Phone;
-    private  int  N_Class;
-    private  String  Trainer;
+public class Student extends Person {
+    private static   String  Trainer;
 
-    public Student(int id, String firstName, String lastName, String email, String phone, int n_Class, String trainer) {
-        Id = id;
-        FirstName = firstName;
-        LastName = lastName;
-        Email = email;
-        Phone = phone;
-        N_Class = n_Class;
-        Trainer = trainer;
+    public Student(int id, String firstName, String lastName, String email, int phone, int n_Class,String trainer) {
+        super(id, firstName, lastName, email, phone, n_Class);
+        this.Trainer=trainer;
     }
 
-    public int getId() {
-        return Id;
-    }
 
-    public void setId(int id) {
-        Id = id;
-    }
-
-    public String getFirstName() {
-        return FirstName;
-    }
-
-    public void setFirstName(String firstName) {
-        FirstName = firstName;
-    }
-
-    public String getLastName() {
-        return LastName;
-    }
-
-    public void setLastName(String lastName) {
-        LastName = lastName;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public String getPhone() {
-        return Phone;
-
-    }
-
-    public void setPhone(String phone) {
-        Phone = phone;
-    }
-
-    public int getN_Class() {
-        return N_Class;
-    }
-
-    public void setN_Class(int n_Class) {
-        N_Class = n_Class;
-    }
-
-    public String getTrainer() {
-        return Trainer;
-    }
-
-    public void setTrainer(String trainer) {
-        Trainer = trainer;
-    }
 
     @Override
-    public String toString() {
+    public void Display() {
 
-        return "  First Name"+getFirstName()+"\n  Last Name : "+getLastName()
+        System.out.print( "  First Name : "+getFirstName()+"\n  Last Name : "+getLastName()
                 + "\n  Number phone : "+getPhone()+"\n  Email : "
-                +getEmail()+"\n  Trainer : "+getTrainer()+"\n  Class : "+ getN_Class();
+                +getEmail()+"\n  Trainer : "+Trainer+"\n  Class : "+ getN_Class());
     }
 }

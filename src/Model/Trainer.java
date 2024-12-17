429d0729916dd10a3140aee57e741e3282253a6e
mohamed <mohamed@gmail.com>
2024-12-17 12:16:17 +0100

    i make the trainer extend the person class to get shared data

--- src/Model/Trainer.java
+++ src/Model/Trainer.java
@@ -1,4 +1,20 @@
 package Model;
 
-public class Trainer {
+public class Trainer extends Person {
+    private  String salary;
+    private String sp;
+
+    public Trainer(int id, String firstName, String lastName, String email, int phone, int n_Class, String salary, String sp) {
+        super(id, firstName, lastName, email, phone, n_Class);
+        this.salary = salary;
+        this.sp = sp;
+    }
+
+    @Override
+    public void Display() {
+        System.out.print(" The Trainer Full Name is "+getFirstName()+" "+getLastName()+"\n The Email is : "
+                +getEmail()+"\n Phone Number : "+getPhone()+"\n The Class Number : "
+                +getN_Class()+"\n The Salary : "+salary+"The Specialisation : "+sp);
+
+    }
 }

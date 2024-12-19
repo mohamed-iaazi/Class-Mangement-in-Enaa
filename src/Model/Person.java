eb33ec37111787afc4a6e91a0477a2a7e15afb05
mohamed <mohamed@gmail.com>
2024-12-19 12:33:15 +0100

    i remove unnecessary  values

--- src/Model/Person.java
+++ src/Model/Person.java
@@ -6,54 +6,16 @@ public  abstract  class Person {
     private  String FirstName;
     private  String  LastName;
     private  String  Email;
-    private  int  Phone;
-    private static   int  N_Class;
 
-    public Person(){
-
-    }
-
-    public Person(int id, String firstName, String lastName, String email, int phone, int n_Class) {
-
+    public Person(int id, String firstName, String lastName, String email) {
         Id = id;
         FirstName = firstName;
         LastName = lastName;
         Email = email;
-        Phone = phone;
-        N_Class = n_Class;
     }
 
-
-    public int getN_Class() {
-        return N_Class;
-    }
-
-    public void setN_Class(int n_Class) {
-        N_Class = n_Class;
-    }
-
-    public int getPhone() {
-        return Phone;
-    }
-
-    public void setPhone(int phone) {
-        Phone = phone;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public String getLastName() {
-        return LastName;
-    }
-
-    public void setLastName(String lastName) {
-        LastName = lastName;
+    public int getId() {
+        return Id;
     }
 
     public String getFirstName() {
@@ -64,18 +26,30 @@ public  abstract  class Person {
         FirstName = firstName;
     }
 
-    public int getId() {
-        return Id;
+    public String getLastName() {
+        return LastName;
     }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public Person(){
+
+    }
+
+
 
     public void setId(int id) {
         Id = id;
     }
-    public   abstract boolean Display();
-    public   abstract void Update(int Id, String f, String l, String Email, int Phone, int N_Class, String Trainer);
-    public   abstract void Delete(int Id);
-    public   abstract void Add(int Id, String f, String l, String Email, int Phone, int N_Class, String Trainer);
-    public   abstract void Update(int Id, String f, String l, String Email, int Phone,int Salary,String sp,int N_Class);
-
 
 }

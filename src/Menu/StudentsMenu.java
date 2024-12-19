48bb03b13907f712e2878eaa213e897c1e06a2fc
mohamed <mohamed@gmail.com>
2024-12-19 10:58:45 +0100

    i use lambda in Onclick method  from interface

--- src/Menu/StudentsMenu.java
+++ src/Menu/StudentsMenu.java
@@ -1,39 +1,50 @@
 package Menu;
 
+import Model.Crud;
+import Model.Student;
 import Utils.Input;
 import interfaces.OnClickListnner;
 
-public class StudentsMenu implements OnClickListnner {
+public class StudentsMenu extends Crud implements OnClickListnner {
+    // var
+    static  private int Id;
+    static  boolean error=false;
+    static Student student = new Student();
 
+
+    // Sub Menu of student
     public static void Menu() {
         int choose=0;
         while (choose != 5) {
             System.out.println("""
                     
-                                *******************************************
-                                *                                         *
-                                *                                         *
-                                *             Student Menu                *
-                                *                                         *
-                                *                                         *
-                                *******************************************
                     
                     
-                                      (1) Add New Student
-                                      (2) Delete Student
-                                      (3) Update Student
-                                      (4) Display All Students
-                                      (5) Back To Main Menu
-                    
-                    
-                    
+                            *******************************************
+                            *                                         *
+                            *                                         *
+                            *             Student Menu                *
+                            *                                         *
+                            *                                         *
+                            *******************************************
+                
+                
+                                  (1) Add New Student
+                                  (2) Delete Student
+                                  (3) Update Student
+                                  (4) Display All Students
+                                  (5) Back To Main Menu
                     
                     """);
             try {
                 System.out.print("   Enter Your Choice : ");
                 choose = Integer.parseInt(Input.getinput());
+                StudentsMenu studentsMenu=new StudentsMenu();
+                studentsMenu.OnClick(choose);
             }
             catch (Exception e){
+                System.out.println("\n\n   An Error Occurred " +e);
+
             }
         }
         }
@@ -41,11 +52,94 @@ public class StudentsMenu implements OnClickListnner {
     @Override
     public void OnClick(int menu) {
         switch (menu){
-            case 1 -> Crud.StudentMenu();
-            case 2 -> application.TrainerMenu();
-            case 3 -> application.ClassMenu();
-            case 4 -> System.exit(0);
+            case 1 -> Add();
+            case 2 -> Delete();
+            case 3 -> Update();
+            case 4 -> Display();
+            case 5 -> System.exit(0);
         }
 
     }
+
+    @Override
+    public void Display() {
+        student.Display();
+    }
+
+    @Override
+    public void Add() {
+
+        do {
+            try {
+                System.out.print("     Enter The First name of the Student: ");
+                String FirstName= Input.getinput();
+                System.out.print("     Enter The Last name of the Student: ");
+                String LastName=Input.getinput();
+                System.out.print("     Enter The Email of the Student: ");
+                String Email=Input.getinput();
+                System.out.print("     Enter The Phone of the Student :");
+                int Phone=Integer.parseInt(Input.getinput());
+                System.out.print("     Enter The Class of the Student :");
+                int N_Class=Integer.parseInt(Input.getinput());
+                System.out.print("     Enter The Trainer of the Student :");
+                String Trainer=Input.getinput();
+                Id=Id+1;
+                student.Add(Id,FirstName,LastName,Email,Phone,N_Class,Trainer);
+                error=false;
+            }
+            catch (Exception e){
+                System.out.println("\n\n   Oops An Error Occurred  "+ e);
+                error=true;
+            }
+        }
+        while (error);
+
+    }
+
+    @Override
+    public void Delete() {
+        System.out.print("Enter The ID Of the Student : ");
+        int id=Integer.parseInt(Input.getinput());
+        student.Delete(id);
+    }
+
+    @Override
+    public void Update() {
+        error=false;
+        if (student.Display()) {
+            do {
+                try {
+                    System.out.println("\n\n   update try!");
+                    System.out.print("     Enter The First name of the Student: ");
+                    String FirstName= Input.getinput();
+                    System.out.print("     Enter The Last name of the Student: ");
+                    String LastName=Input.getinput();
+                    System.out.print("     Enter The Email of the Student: ");
+                    String Email=Input.getinput();
+                    System.out.print("     Enter The Phone of the Student :");
+                    int Phone=Integer.parseInt(Input.getinput());
+                    System.out.print("     Enter The Class of the Student :");
+                    int N_Class=Integer.parseInt(Input.getinput());
+                    System.out.print("     Enter The Trainer of the Student :");
+                    String Trainer=Input.getinput();
+                    Id=Id+1;
+
+                    student.Update(Id,FirstName,LastName,Email,Phone,N_Class,Trainer);
+                    error=false;
+                }
+                catch (Exception e){
+                    System.out.println("  Oops An Error Occured  ");
+                    error=true;
+                }
+            }
+            while (error);
+
+        }
+        else {
+            System.out.println("\n\n   No Data To Update");
+
+    }
+    }
+
+
 }
--- src/Menu/TrainersMenu.java
+++ src/Menu/TrainersMenu.java
@@ -1,6 +1,149 @@
 package Menu;
 
-public class TrainersMenu {
+import Model.Crud;
+import Model.Student;
+import Model.Trainer;
+import Utils.Input;
+import interfaces.OnClickListnner;
+
+public class TrainersMenu extends Crud implements OnClickListnner {
+
+    static  private int Id;
+    static  boolean error=false;
+    static Trainer trainer = new Trainer();
+
     public static void Menu() {
+        int choose=0;
+        while (choose != 5) {
+            System.out.println("""
+                    
+                      
+                                       *******************************************
+                                       *                                         *
+                                       *                                         *
+                                       *             Trainer Menu                *
+                                       *                                         *
+                                       *                                         *
+                                       *******************************************
+                                     
+                                                  (1) Add New Trainer
+                                                  (2) Delete Trainer
+                                                  (3) Update Trainer info
+                                                  (4) Associated To class
+                                                  (5) Display All Trainers
+                                                  (5) Back To Main Menu
+                                                  
+                    
+                    """);
+            try {
+                System.out.print("   Enter Your Choice : ");
+                choose = Integer.parseInt(Input.getinput());
+            }
+            catch (Exception e){
+                System.out.println("\n\n   An Error Occurred " +e);
+
+            }
+        }
     }
+
+
+
+    @Override
+    public void OnClick(int menu) {
+        switch (menu){
+            case 1 -> Add();
+            case 2 -> Delete();
+            case 3 -> Update();
+            case 4 -> Associate();
+            case 5 -> Display();
+            case 6 -> System.exit(0);
+        }
+    }
+
+
+    @Override
+    public void Add() {
+        do {
+            try {
+                System.out.print("     Enter The First name of the Trainer: ");
+                String FirstName = Input.getinput();
+                System.out.print("     Enter The Last name of the Trainer: ");
+                String LastName = Input.getinput();
+                System.out.print("     Enter The Email of the Trainer: ");
+                String Email = Input.getinput();
+                System.out.print("     Enter The Phone of the Trainer :");
+                int Phone = Integer.parseInt(Input.getinput());
+                System.out.print("     Enter The Class of the Trainer :");
+                int N_Class = Integer.parseInt(Input.getinput());
+                System.out.print("     Enter The Salary of the Trainer :");
+                String salary = Input.getinput();
+                Id = Id + 1;
+                trainer.Add(Id, FirstName, LastName, Email, Phone, N_Class, salary);
+                error = false;
+            } catch (Exception e) {
+                System.out.println("\n\n   Oops An Error Occurred  ");
+                error = true;
+            }
+        }
+        while (error);
+    }
+
+    @Override
+    public void Delete() {
+        System.out.print("Enter The ID Of the Trainer : ");
+        int id = Integer.parseInt(Input.getinput());
+        trainer.Delete(id);
+
+    }
+
+    @Override
+    public void Update() {
+        error = false;
+
+        do {
+            try {
+                System.out.println("\n\n   update try!");
+                System.out.print("     Enter The First name of the Trainer: ");
+                String FirstName = Input.getinput();
+                System.out.print("     Enter The Last name of the Trainer: ");
+                String LastName = Input.getinput();
+                System.out.print("     Enter The Email of the Student: ");
+                String Email = Input.getinput();
+                System.out.print("     Enter The Phone of the Student :");
+                int Phone = Integer.parseInt(Input.getinput());
+                System.out.print("     Enter The Class of the Student :");
+                int N_Class = Integer.parseInt(Input.getinput());
+                System.out.print("     Enter The Trainer of the Student :");
+                String Trainer = Input.getinput();
+                Id = Id + 1;
+
+                trainer.Update(Id, FirstName, LastName, Email, Phone, N_Class, Trainer);
+                error = false;
+            } catch (Exception e) {
+                System.out.println("\n\n   Oops An Error Occurred  ");
+                error = true;
+            }
+        }
+        while (error);
+
+
+
+    }
+    public void Associate() {
+        // get the user iid to update
+        System.out.print("\n\n   Enter The Id of The trainer  : ");
+        int id=Integer.parseInt(Input.getinput());
+        System.out.print("\n\n   Enter The Number Of The Class You Want To associate : ");
+        int c=Integer.parseInt(Input.getinput());
+        // call the assosiate function in the trainer model  adn it takes two param
+        Trainer.ass(id,c);
+    }
+
+
+    @Override
+    public void Display() {
+        trainer.Display();
+    }
+
+
 }

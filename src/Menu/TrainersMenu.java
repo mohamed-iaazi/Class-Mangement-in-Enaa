afb107b3eeb1b12649355c18a660ec3d70ac6877
mohamed <mohamed@gmail.com>
2024-12-20 15:18:41 +0100

    i fix the add and delete and display functions

--- src/Menu/TrainersMenu.java
+++ src/Menu/TrainersMenu.java
@@ -6,15 +6,18 @@ import Model.Trainer;
 import Utils.Input;
 import interfaces.OnClickListnner;
 
+import java.util.ArrayList;
+
 public class TrainersMenu extends Crud implements OnClickListnner {
 
     static  private int Id;
     static  boolean error=false;
-    static Trainer trainer = new Trainer();
+    static ArrayList<Trainer> trainerArrayList =new ArrayList<>();
+
 
     public static void Menu() {
         int choose=0;
-        while (choose != 5) {
+        while (choose != 6) {
             System.out.println("""
                     
                       
@@ -31,13 +34,15 @@ public class TrainersMenu extends Crud implements OnClickListnner {
                                                   (3) Update Trainer info
                                                   (4) Associated To class
                                                   (5) Display All Trainers
-                                                  (5) Back To Main Menu
+                                                  (6) Back To Main Menu
                                                   
                     
                     """);
             try {
                 System.out.print("   Enter Your Choice : ");
                 choose = Integer.parseInt(Input.getinput());
+                TrainersMenu trainersMenu=new TrainersMenu();
+                trainersMenu.OnClick(choose);
             }
             catch (Exception e){
                 System.out.println("\n\n   An Error Occurred " +e);
@@ -56,7 +61,7 @@ public class TrainersMenu extends Crud implements OnClickListnner {
             case 3 -> Update();
             case 4 -> Associate();
             case 5 -> Display();
-            case 6 -> System.exit(0);
+            case 6 -> System.out.print("\n\n    Back To Main Menu"); //back;
         }
     }
 
@@ -71,14 +76,19 @@ public class TrainersMenu extends Crud implements OnClickListnner {
                 String LastName = Input.getinput();
                 System.out.print("     Enter The Email of the Trainer: ");
                 String Email = Input.getinput();
-                System.out.print("     Enter The Phone of the Trainer :");
-                int Phone = Integer.parseInt(Input.getinput());
-                System.out.print("     Enter The Class of the Trainer :");
-                int N_Class = Integer.parseInt(Input.getinput());
                 System.out.print("     Enter The Salary of the Trainer :");
-                String salary = Input.getinput();
-                Id = Id + 1;
-               // trainer.Add(Id, FirstName, LastName, Email, Phone, N_Class, salary);
+                int salary = Integer.parseInt(Input.getinput());
+                System.out.print("     Enter The Specialisation of the Trainer :");
+                String sp = Input.getinput();
+                if (!trainerArrayList.isEmpty()){
+                    Id= trainerArrayList.getLast().getId()+1;
+                }
+                else {
+                    Id=Id+1;
+                }
+                trainerArrayList.add(new Trainer(Id,FirstName,LastName,Email,salary,sp));
+
+
                 error = false;
             } catch (Exception e) {
                 System.out.println("\n\n   Oops An Error Occurred  ");
@@ -87,44 +97,89 @@ public class TrainersMenu extends Crud implements OnClickListnner {
         }
         while (error);
     }
-
     @Override
     public void Delete() {
-        System.out.print("Enter The ID Of the Trainer : ");
-        int id = Integer.parseInt(Input.getinput());
-        //trainer.Delete(id);
 
-    }
 
-    @Override
-    public void Update() {
-        error = false;
+        if (!trainerArrayList.isEmpty()){
 
-        do {
-            try {
-                System.out.println("\n\n   update try!");
-                System.out.print("     Enter The First name of the Trainer: ");
-                String FirstName = Input.getinput();
-                System.out.print("     Enter The Last name of the Trainer: ");
-                String LastName = Input.getinput();
-                System.out.print("     Enter The Email of the Student: ");
-                String Email = Input.getinput();
-                System.out.print("     Enter The Phone of the Student :");
-                int Phone = Integer.parseInt(Input.getinput());
-                System.out.print("     Enter The Class of the Student :");
-                int N_Class = Integer.parseInt(Input.getinput());
-                System.out.print("     Enter The Trainer of the Student :");
-                String Trainer = Input.getinput();
-                Id = Id + 1;
 
-              //  trainer.Update(Id, FirstName, LastName, Email, Phone, N_Class, Trainer);
-                error = false;
-            } catch (Exception e) {
-                System.out.println("\n\n   Oops An Error Occurred  ");
-                error = true;
+            boolean found = false;
+            System.out.print("Enter The ID Of the Trainer : ");
+            int id = Integer.parseInt(Input.getinput());
+
+            for (int i = 0; i < trainerArrayList.size(); i++) {
+                if (id == trainerArrayList.get(i).getId()) {
+                    found = true;
+                    id=i;
+                }
+            }
+            if (found) {
+                trainerArrayList.remove(id);
+                System.out.println("\n\n      Deleted With Success");
+
+            } else {
+                System.out.println("\n\n      Ops Not Found");
             }
         }
-        while (error);
+        else {
+            System.out.println("\n\n   No Trainer To Delete");
+        }
+
+    }
+    @Override
+    public void Update() {
+        error=false;
+        boolean found=false;
+        int origineid=0;
+        if (!trainerArrayList.isEmpty()) {
+            do {
+                try {
+                    System.out.println("\n\n   Enter The Id Of Trainer To Update : ");
+                    int id=Integer.parseInt(Input.getinput());
+                    origineid=id;
+
+                    for(int i =0 ; i<trainerArrayList.size();i++){
+                        if (id==trainerArrayList.get(i).getId())
+                        {
+                            found=true;
+                            id=i;
+                        }
+                    }
+                    if (found){
+                        System.out.print("     Enter The First name of the Trainer: ");
+                        String FirstName = Input.getinput();
+                        System.out.print("     Enter The Last name of the Trainer: ");
+                        String LastName = Input.getinput();
+                        System.out.print("     Enter The Email of the Trainer: ");
+                        String Email = Input.getinput();
+                        System.out.print("     Enter The Salary of the Trainer :");
+                        int salary = Integer.parseInt(Input.getinput());
+                        System.out.print("     Enter The Specialisation of the Trainer :");
+                        String sp = Input.getinput();
+                        trainerArrayList.set(id,new Trainer(origineid,FirstName,LastName,Email,salary,sp));
+                        System.out.println("\n\n   Your Student Update With Success");
+                        error=false;
+                    }
+                    else {
+                        System.out.println("\n\n   The ID Doesn't Match Ant Student ");
+                        error=true;
+
+                    }
+
+                }
+                catch (Exception e){
+                    System.out.println("  Oops An Error Occurred  ");
+                    error=true;
+                }
+            }
+            while (error);
+
+        }
+        else {
+            System.out.println("\n\n   No Data To Update");
+
+        }
 
 
 
@@ -138,11 +193,26 @@ public class TrainersMenu extends Crud implements OnClickListnner {
         // call the assosiate function in the trainer model  adn it takes two param
       //  Trainer.ass(id,c);
     }
-
-
     @Override
     public void Display() {
-       // trainer.Display();
+
+
+        if (trainerArrayList.isEmpty()) {
+            System.out.println("\n\n   Ops No Student To Display ");
+        } else {
+            for (Trainer value : trainerArrayList) {
+                Trainer trainer;
+                trainer = value;
+
+                System.out.print("\n\n  The Id : " + trainer.getId()
+                        + "\n  First Name : " + trainer.getFirstName()
+                        + "\n  Last Name : " + trainer.getLastName()
+                        + "\n  Email : " + trainer.getEmail()
+                        + "\n  The Salary Is : " + trainer.getSalary()
+                        + "\n  The Specialisation is : " + trainer.getSp());
+            }
+
+        }
     }
 
 
